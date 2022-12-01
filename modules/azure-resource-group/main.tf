264e0448db68dfccf90c1fc4fabbc70926a1f779
Yan <yjmrobert@gmail.com>
2022-12-01 13:38:30 -0500

    removed purge protection and moves access policy

--- modules/azure-resource-group/main.tf
+++ modules/azure-resource-group/main.tf
@@ -14,7 +14,7 @@ resource "azurerm_key_vault" "az_proj_kv" {
   enabled_for_disk_encryption     = true
   tenant_id                       = var.az_tenant_id
   soft_delete_retention_days      = 90
-  purge_protection_enabled        = true
+  purge_protection_enabled        = false
   enabled_for_template_deployment = true
 
   sku_name = "standard"
@@ -23,28 +23,6 @@ resource "azurerm_key_vault" "az_proj_kv" {
     var.common_tags,
     { "environment_name" : var.environment_name }
   )
-
-  access_policy {
-    tenant_id = var.az_tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-
-    key_permissions = [
-      "Get",
-      "List",
-      "UnwrapKey",
-      "WrapKey",
-      "Encrypt",
-      "Decrypt",
-      "Sign",
-      "Verify",
-      "Create"
-    ]
-
-    secret_permissions = [
-      "List",
-      "Get"
-    ]
-  }
 }
 
 resource "azurerm_key_vault_key" "az_proj_cmk" {
@@ -61,4 +39,13 @@ resource "azurerm_key_vault_key" "az_proj_cmk" {
     "verify",
     "wrapKey"
   ]
+}
+
+resource "azurerm_key_vault_access_policy" "kv_creator_policy" {
+  key_vault_id = azurerm_key_vault.az_proj_kv.id
+  tenant_id    = var.az_tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  key_permissions    = ["Get", "List", "UnwrapKey", "WrapKey", "Encrypt", "Decrypt", "Sign", "Verify", "Create"]
+  secret_permissions = ["List", "Get"]
 }
