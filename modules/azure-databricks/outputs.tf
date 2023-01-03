1a96c79ba4823872cec0db21f634c5e544e83a22
Yan <yjmrobert@gmail.com>
2023-01-03 08:30:04 -0500

    :bug: added workspace url output

--- modules/azure-databricks/outputs.tf
+++ modules/azure-databricks/outputs.tf
@@ -1,7 +1,11 @@
-output "databricks_workspace_id" {
+output "azure_databricks_workspace_id" {
   value = azurerm_databricks_workspace.datahub_databricks_workspace.id
 }
 
-output "databricks_workspace_name" {
+output "azure_databricks_workspace_name" {
   value = azurerm_databricks_workspace.datahub_databricks_workspace.name
+}
+
+output "azure_databricks_workspace_url" {
+  value = azurerm_databricks_workspace.datahub_databricks_workspace.workspace_url
 }
