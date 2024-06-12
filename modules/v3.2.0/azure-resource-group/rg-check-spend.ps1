6b85741512d74d00c2836f54dcd99c35a9d16582
John Bain <john.bain@gmail.com>
2024-06-12 17:04:01 +0000

    removed divide by zero conditions

--- modules/v3.2.0/azure-resource-group/rg-check-spend.ps1
+++ modules/v3.2.0/azure-resource-group/rg-check-spend.ps1
@@ -16,8 +16,22 @@ $currentSpendMain = (Get-AzConsumptionBudget -name $budget_name).currentspend.am
 $currentSpendDbr = 0
 try { $currentSpendDbr = (Get-AzConsumptionBudget -name $budget_name_dbr).currentspend.amount } catch { $currentSpendDbr = 0 }
 
+
+if ($budgetAmount -eq 0) {
+    Write-Output "No valid budgets found. Exiting script to avoid divide by zero."
+    exit 0
+}
+
+if ($currentSpendMain -eq 0) {
+    Write-Output "No valid spend found. Exiting script to avoid divide by zero."
+    exit 0
+}
+
+
 $totalSpendDollar = [math]::round(($currentSpendMain + $currentSpendDbr ))
 $totalPercent = [math]::round((($currentSpendMain + $currentSpendDbr ) / $budgetAmount) * 100, 2)
+
+
 $currentSpendMainDollar = [math]::round($currentSpendMain)
 $currentSpendDbrDollar = [math]::round($currentSpendDbr)
 Write-Output "Current budgets: $budget_name and $budget_name_dbr combined is $budgetAmount (trigger is $trigger_percent percent)"
