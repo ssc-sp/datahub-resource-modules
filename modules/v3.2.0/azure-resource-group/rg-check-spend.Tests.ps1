2ee8da395ed4ca545aec6ab78368e0e8900a0297
John Bain <john.bain@gmail.com>
2024-06-12 18:45:29 +0000

    fasdvdxzfsgh

--- modules/v3.2.0/azure-resource-group/rg-check-spend.Tests.ps1
+++ modules/v3.2.0/azure-resource-group/rg-check-spend.Tests.ps1
@@ -4,7 +4,6 @@ Import-Module Pester
 # Describe block for the tests
 Describe 'Budget Script Tests' {
 
-
     BeforeAll {
         # Create a function so the az module doesn't have to be downloaded and installed
         function Connect-AzAccount () {}
@@ -19,45 +18,11 @@ Describe 'Budget Script Tests' {
             }
         }
 
-
     
-        # Mock the Azure cmdlets
-        Mock -CommandName Connect-AzAccount -MockWith {
-            Write-Host "Mock Connect-AzAccount called"
-        }
 
-        Mock -CommandName Get-AzConsumptionBudget -MockWith {
-            param ($name)
-            if ($name -eq "TestBudget") {
-                return [PSCustomObject]@{ Amount = 1000; currentspend = [PSCustomObject]@{ amount = 500 } }
-            } elseif ($name -eq "TestBudgetDbr") {
-                return [PSCustomObject]@{ Amount = 1000; currentspend = [PSCustomObject]@{ amount = 300 } }
-            }
-        }
 
     }
 
-
-
-
-    # Test to verify calculations
-    It 'Calculates total spend and percentage correctly' {
-        # Set parameters
-        $params = @{
-            trigger_percent = 80
-            key_vault_name = "TestKeyVault"
-            budget_name = "TestBudget"
-            budget_name_dbr = "TestBudgetDbr"
-            subscription_id = "TestSubscriptionId"
-        }
-
-        # Execute the script
-        . "modules/v3.2.0/azure-resource-group/rg-check-spend.ps1" @params
-
-        # Assertions
-        $totalSpendDollar | Should -Be 800
-        $totalPercent | Should -Be 80
-    }
 
 
 }
