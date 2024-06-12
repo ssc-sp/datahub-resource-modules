bcb8a8072242f60047a0c6a727b11bfd77b0df90
John Bain <john.bain@gmail.com>
2024-06-12 17:04:17 +0000

    updated test

--- modules/v3.2.0/azure-resource-group/rg-check-spend.Tests.ps1
+++ modules/v3.2.0/azure-resource-group/rg-check-spend.Tests.ps1
@@ -4,10 +4,39 @@ Import-Module Pester
 # Describe block for the tests
 Describe 'Budget Script Tests' {
 
-    # Mock the Azure cmdlets
-    Mock -CommandName Connect-AzAccount -MockWith {
-        Write-Host "Mock Connect-AzAccount called"
+
+    BeforeAll {
+        # Create a function so the az module doesn't have to be downloaded and installed
+        function Connect-AzAccount () {}
+
+        function Get-AzConsumptionBudget {
+            param (
+                [string]$name
+            )
+            return [PSCustomObject]@{
+                Amount       = 0
+                currentspend = [PSCustomObject]@{ amount = 0 }
+            }
+        }
+
+
+    
+        # Mock the Azure cmdlets
+        Mock -CommandName Connect-AzAccount -MockWith {
+            Write-Host "Mock Connect-AzAccount called"
+        }
+
+        Mock -CommandName Get-AzConsumptionBudget -MockWith {
+            param ($name)
+            if ($name -eq "TestBudget") {
+                return [PSCustomObject]@{ Amount = 1000; currentspend = [PSCustomObject]@{ amount = 500 } }
+            } elseif ($name -eq "TestBudgetDbr") {
+                return [PSCustomObject]@{ Amount = 1000; currentspend = [PSCustomObject]@{ amount = 300 } }
+            }
+        }
+
     }
+
 
 
 
