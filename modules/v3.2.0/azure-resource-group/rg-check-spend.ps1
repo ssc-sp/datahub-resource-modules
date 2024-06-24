7023bbc2f193974fb5d9cf4dd4f6b9bcd3d2347f
John Bain <john.bain@gmail.com>
2024-06-24 18:15:30 +0000

    force it

--- modules/v3.2.0/azure-resource-group/rg-check-spend.ps1
+++ modules/v3.2.0/azure-resource-group/rg-check-spend.ps1
@@ -6,11 +6,11 @@ param (
     [string]$subscription_id
 )
 
-Install-Module -Name Az.Accounts -RequiredVersion 2.19.0 
-Import-Module -Name Az.Accounts -RequiredVersion 2.19.0 
+Install-Module -Name Az.Accounts -RequiredVersion 2.19.0  -Force
+Import-Module -Name Az.Accounts -RequiredVersion 2.19.0 -Force
 
-Install-Module -Name Az.KeyVault -RequiredVersion 5.3.0 
-Import-Module -Name Az.KeyVault -RequiredVersion 5.3.0 
+Install-Module -Name Az.KeyVault -RequiredVersion 5.3.0 -Force
+Import-Module -Name Az.KeyVault -RequiredVersion 5.3.0 -Force
 
 function Connect-ToAzureIdentity {
     param (
