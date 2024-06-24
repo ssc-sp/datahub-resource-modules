cc55ebfa6182e50b69cc5843d545a38c200824be
Simon Wang <simon.wang@ssc-spc.gc.ca>
2024-06-24 09:23:07 -0400

    fix hardcoded sub id

--- modules/v3.2.0/azure-resource-group/rg-check-spend.ps1
+++ modules/v3.2.0/azure-resource-group/rg-check-spend.ps1
@@ -7,7 +7,7 @@ $key_vault_name = "${key_vault_name}"
 $budget_name = "${budget_name}"
 $dbr_rg_name = "${dbr_rg_name}"
 
-Connect-AzAccount -Identity -Subscription "f42cb866-f2c8-4a25-a260-9e3d7e01f8bb"
+Connect-AzAccount -Identity -Subscription "${subscription_id}"
 
 $budget = (Get-AzConsumptionBudget -name $budget_name)
 $startDate = $budget.timePeriod.startDate.toUniversalTime()
