dba86f1b24e5100e7a9b5b8d1a44e845edc3278d
Simon Wang <41697802+simon-wang-gc@users.noreply.github.com>
2024-08-12 12:46:49 -0400

    add no_public_ip to databricks (#131)
    Co-authored-by: Simon Wang <simon.wang@ssc-spc.gc.ca>

--- modules/v3.4.0/azure-databricks/main.tf
+++ modules/v3.4.0/azure-databricks/main.tf
@@ -7,6 +7,10 @@ resource "azurerm_databricks_workspace" "datahub_databricks_workspace" {
   customer_managed_key_enabled      = true
   infrastructure_encryption_enabled = true
 
+  custom_parameters {
+    no_public_ip = false
+  }
+
   tags = local.project_tags
 }
 
