8c2f411a7546e321bc9407831457666dcc2de3b6
Simon Wang <simon.wang@ssc-spc.gc.ca>
2024-10-22 15:53:52 -0400

    disable defender for storage to override subscription settings

--- modules/v4.0.0/azure-storage-blob/av.tf
+++ modules/v4.0.0/azure-storage-blob/av.tf
@@ -1,7 +1,7 @@
 resource "azurerm_security_center_storage_defender" "datahub_storageaccount_av" {
   storage_account_id = azurerm_storage_account.datahub_storageaccount.id
 
-  override_subscription_settings_enabled      = true # Optional
-  malware_scanning_on_upload_enabled          = true
+  override_subscription_settings_enabled      = true  # Optional
+  malware_scanning_on_upload_enabled          = false # Disable for unclassified
   malware_scanning_on_upload_cap_gb_per_month = -1
 }
