0d85e7a7fc9579da9d3344dc52604b510f37592d
Alex Khavich <alexk@apption.com>
2025-07-03 13:10:45 -0400

    ML and ML+GPU clusters with corresponding policies

--- modules/azure-databricks/cluster.tf
+++ modules/azure-databricks/cluster.tf
@@ -13,6 +13,21 @@ data "databricks_spark_version" "dbk_latest_lts" {
   depends_on = [azurerm_databricks_workspace.datahub_databricks_workspace]
 }
 
+data "databricks_spark_version" "dbk_latest_lts_ml" {
+  long_term_support = true
+  ml = true
+
+  depends_on = [azurerm_databricks_workspace.datahub_databricks_workspace]
+}
+
+data "databricks_spark_version" "dbk_latest_lts_ml_gpu" {
+  long_term_support = true
+  ml = true
+  gpu = true
+
+  depends_on = [azurerm_databricks_workspace.datahub_databricks_workspace]
+}
+
 # Serverless cluster profile: "spark.databricks.cluster.profile" : "serverless",
 resource "databricks_cluster" "dbk_proj_cluster" {
   cluster_name            = "main_cluster"
@@ -34,6 +49,54 @@ resource "databricks_cluster" "dbk_proj_cluster" {
   autoscale {
     min_workers = 0
     max_workers = 2
+  }
+}
+
+resource "databricks_cluster" "dbk_proj_cluster_ml" {
+  cluster_name            = "ml_cluster"
+  spark_version           = data.databricks_spark_version.dbk_latest_lts_ml.id
+  node_type_id            = "Standard_E8ds_v5"
+  driver_node_type_id     = "Standard_E8ds_v5"
+  autotermination_minutes = 30
+  num_workers             = 1
+  is_pinned               = true
+  policy_id               = databricks_cluster_policy.ml_policy.id
+
+  spark_conf = {
+    "spark.databricks.passthrough.enabled" : "true",
+    "spark.databricks.delta.preview.enabled" : "true",
+    "spark.databricks.repl.allowedLanguages" : "python,sql,r"
+  }
+
+  autoscale {
+    min_workers = 1
+    max_workers = 4
+  }
+}
+
+resource "databricks_cluster" "dbk_proj_cluster_ml_gpu" {
+  cluster_name            = "gpu_cluster"
+  spark_version           = data.databricks_spark_version.dbk_latest_lts_ml_gpu.id
+  node_type_id            = "Standard_NV36ads_A10_v5"
+  driver_node_type_id     = "Standard_NV36ads_A10_v5"
+  autotermination_minutes = 30
+  num_workers             = 1
+  is_pinned               = true
+  policy_id               = databricks_cluster_policy.ml_gpu_policy.id
+
+  spark_conf = {
+    "spark.databricks.passthrough.enabled" : "true",
+    "spark.databricks.delta.preview.enabled" : "true",
+    "spark.databricks.repl.allowedLanguages" : "python,sql,r"
+    "spark.task.resource.gpu.amount"                 = "1"
+    "spark.executor.resource.gpu.amount"             = "1"
+    "spark.executor.resource.gpu.discoveryScript"    = "/databricks/spark/scripts/gpu/getGpusResources.sh"
+    "spark.resources.discoveryScript"                = "/databricks/spark/scripts/gpu/getGpusResources.sh"
+  }
+
+  autoscale {
+    min_workers = 1
+    max_workers = 4
   }
 }
 
