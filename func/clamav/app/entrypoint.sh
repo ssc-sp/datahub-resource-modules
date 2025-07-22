50bf109fe930696b65a45ef6df30b5a5d6883ed2
Simon Wang <simon.wang@ssc-spc.gc.ca>
2025-07-22 16:21:09 -0400

    run python pip in docker image

--- func/clamav/app/entrypoint.sh
+++ func/clamav/app/entrypoint.sh
@@ -3,6 +3,4 @@
 touch $0.start
 freshclam
 
-python3 -m venv fsdh && . fsdh/bin/activate && \
-    pip install -r ./requirements.txt && \
-    python3 scan_blob.py
+. fsdh/bin/activate && python3 scan_blob.py
