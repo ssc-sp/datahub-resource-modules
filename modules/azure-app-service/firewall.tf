312833200712307af71dc6c3cab1bb6f29692b05
Simon Wang <simon.wang@ssc-spc.gc.ca>
2026-03-10 13:41:49 -0400

    flattern ip rule for app service

--- modules/azure-app-service/firewall.tf
+++ modules/azure-app-service/firewall.tf
@@ -1,6 +1,4 @@
 resource "null_resource" "access_ip_whitelist_one" {
-  for_each = { for s in local.allow_source_ip_list : index(local.allow_source_ip_list, s) => s }
-
   triggers = {
     rg_name   = var.resource_group_name
     app_name  = azurerm_linux_web_app.datahub_proj_app.name
@@ -10,10 +8,8 @@ resource "null_resource" "access_ip_whitelist_one" {
   provisioner "local-exec" {
     interpreter = ["pwsh", "-Command"]
     command     = <<-EOT
-      $ip_addr="${each.value}".trim()
-      if ( ! [string]::IsNullOrEmpty("$ip_addr") ) { 
-        $cidr = $ip_addr -match "/" ? $ip_addr : "$ip_addr/32"        
-        az webapp config access-restriction add -g ${var.resource_group_name} -n ${azurerm_linux_web_app.datahub_proj_app.name} --rule-name fsdh-app-${each.key} --action Allow --ip-address "$cidr" --priority ${100 + each.key}
+      if ( ! [string]::IsNullOrEmpty("${var.allow_source_ip}") ) { 
+        az webapp config access-restriction add -g ${var.resource_group_name} -n ${azurerm_linux_web_app.datahub_proj_app.name} --rule-name fsdh-app-${each.key} --action Allow --ip-address "${var.allow_source_ip}" --priority ${100 + each.key}
       }
     EOT
     on_failure  = fail
