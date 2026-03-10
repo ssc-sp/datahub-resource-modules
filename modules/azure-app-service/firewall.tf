0c474d4c1b6a6c0c3af5c068f1552da7dd889a46
Simon Wang <simon.wang@ssc-spc.gc.ca>
2026-03-10 14:00:36 -0400

    fw cli error

--- modules/azure-app-service/firewall.tf
+++ modules/azure-app-service/firewall.tf
@@ -19,7 +19,7 @@ resource "null_resource" "access_ip_whitelist_one" {
     when        = destroy
     interpreter = ["pwsh", "-Command"]
     command     = <<-EOT
-      az webapp config access-restriction remove -g ${self.triggers.rg_name} -n ${self.triggers.app_name} --rule-name  fsdh-app-${each.key}
+      az webapp config access-restriction remove -g ${var.resource_group_name} -n ${azurerm_linux_web_app.datahub_proj_app.name} --rule-name fsdh-app-trusted 
     EOT
     on_failure  = continue
   }
