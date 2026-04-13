51a737566e079c8081f48f1c93d6cbc6c3f30465
simon wang <simon.wang@ssc-spc.gc.ca>
2026-04-13 21:08:19 +0000

    wip - test security profile

--- enhanced/modules/azure-databricks/main.tf
+++ enhanced/modules/azure-databricks/main.tf
@@ -9,11 +9,6 @@ resource "azurerm_databricks_workspace" "datahub_databricks_workspace" {
   public_network_access_enabled         = local.public_access
   network_security_group_rules_required = local.public_access ? "AllRules" : "NoAzureDatabricksRules"
 
-  # enhanced_security_compliance {
-  #   compliance_security_profile_enabled   = true
-  #   compliance_security_profile_standards = ["CANADA_PROTECTED_B"]
-  # }
-
   custom_parameters {
     no_public_ip                                         = !var.is_dev
     virtual_network_id                                   = data.azurerm_virtual_network.datahub_vnet.id
@@ -36,16 +31,57 @@ resource "databricks_workspace_conf" "datahub_databricks_workspace_conf" {
   }
 }
 
-resource "azapi_update_resource" "canada_protected_b" {
-  type        = "Microsoft.Databricks/workspaces@2025-08-01-preview"
-  resource_id = azurerm_databricks_workspace.datahub_databricks_workspace.id
-
-  body = {
-    properties = {
-      enhandedSecurityCompliance = {
-        complianceSecurityProfile = { value = "Enabled" }
-        complianceStandards       = ["CANADA_PROTECTED_B"]
+resource "null_resource" "datahub_databricks_protected_b" {
+  provisioner "local-exec" {
+    interpreter = ["pwsh", "-Command"]
+    command     = <<-EOT
+      $subscriptionId="${var.az_subscription_id}"
+      $param=@{
+        ResourceGroupName="${var.resource_group_name}"
+        Name="${azurerm_databricks_workspace.datahub_databricks_workspace.name}"
+        EnhancedSecurityCompliance="Enabled"
+        ComplianceStandard=@("CANADA_PROTECTED_B")
+        EnhancedSecurityMonitoring="Enabled"
+        AutomaticClusterUpdate="Enabled"
       }
-    }
+      update-azDatabricksWorkspace -subscriptionid $subscriptionId @param
+    EOT
   }
 }
+
+# data "azurerm_resource_group" "az_project_rg" {
+#   name = var.resource_group_name
+# }
+
+# data "azapi_resource_id" "datahub_databricks_rg" {
+#   type      = "Microsoft.Resources/resourceGroups@2025-08-01-preview"
+#   parent_id = data.azurerm_resource_group.az_project_rg.id
+#   name      = "simontests2602labcdefgh"
+# }
+
+# resource "azapi_resource" "datahub_databricks_workspace" {
+#   type      = "Microsoft.Databricks/workspaces@2025-08-01-preview"
+#   name      = "${local.databricks_name}-1"
+#   parent_id = data.azurerm_resource_group.az_project_rg.id
+#   location  = local.resource_group_location
+#   tags      = var.project_tags
+
+#   body = {
+#     properties = {
+#       managedResourceGroupId = data.azapi_resource_id.datahub_databricks_rg.id
+
+#       parameters = {
+#         prepareEncryption               = { value = true }
+#         requireInfrastructureEncryption = { value = true }
+#         customVirtualNetworkId          = { value = data.azurerm_virtual_network.datahub_vnet.id }
+#         customPublicSubnetName          = { value = var.dbr_subnet_public }
+#         customPrivateSubnetName         = { value = var.dbr_subnet_private }
+#       }
+#       publicNetworkAccess = "Enabled"
+
+#     }
+#     sku = { name = "premium" }
+#   }
+#   schema_validation_enabled = false
+#   response_export_values    = ["*"]
+# }
