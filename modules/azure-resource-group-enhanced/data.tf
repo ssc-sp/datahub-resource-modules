4014240fcb479c96f9eadc13e739a88865ad0a7d
simon.wang@ssc-spc.gc.ca <simon.wang@ssc-spc.gc.ca>
2026-07-13 09:11:14 -0400

    add clamav queue for uniformity

--- modules/azure-resource-group-enhanced/data.tf
+++ modules/azure-resource-group-enhanced/data.tf
@@ -108,6 +108,7 @@ locals {
   datahub_quarantine        = "datahub-quarantine"
   blob_created_queue        = "blob-created"
   blob_muted_queue          = "blob-event-muted"
+  clamav_result_queue       = "clamav-scan-result"
   storage_size_limit_bytes  = 1024 * 1024 * 1024 * 1024 * var.storage_size_limit_tb
   log_keep_days             = 90
 
