a27822922d74bcd594175c955ccb625b2138f386
Simon Wang <41697802+simon-wang-gc@users.noreply.github.com>
2026-07-24 09:46:22 -0400

    tested app service firewall (#272)

--- modules/azure-app-service/firewall.tf
+++ modules/azure-app-service/firewall.tf
@@ -1,26 +1,11 @@
-resource "null_resource" "access_ip_whitelist_one" {
-  triggers = {
-    rg_name   = var.resource_group_name
-    app_name  = azurerm_linux_web_app.datahub_proj_app.name
-    source_ip = var.allow_source_ip
-  }
+resource "azapi_update_resource" "fsdh_app_ip_restrictions" {
+  type      = "Microsoft.Web/sites/config@2025-03-01"
+  name      = "web"
+  parent_id = azurerm_linux_web_app.datahub_proj_app.id
 
-  provisioner "local-exec" {
-    interpreter = ["pwsh", "-Command"]
-    command     = <<-EOT
-      if ( ! [string]::IsNullOrEmpty("${var.allow_source_ip}") ) { 
-        az webapp config access-restriction add -g ${var.resource_group_name} -n ${azurerm_linux_web_app.datahub_proj_app.name} --rule-name fsdh-app-trusted --action Allow --ip-address "${var.allow_source_ip}" --priority 100
-      }
-    EOT
-    on_failure  = fail
-  }
-
-  provisioner "local-exec" {
-    when        = destroy
-    interpreter = ["pwsh", "-Command"]
-    command     = <<-EOT
-      az webapp config access-restriction remove -g ${self.triggers.rg_name} -n ${self.triggers.app_name} --rule-name fsdh-app-trusted 
-    EOT
-    on_failure  = continue
+  body = {
+    properties = {
+      ipSecurityRestrictions = local.allow_source_ip_list
+    }
   }
 }
